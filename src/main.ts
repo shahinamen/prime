aa1c3105c727880e3d85287c8ffcc99f4940a3b4
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-05-19 08:34:52 +0600

    primevue installed

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue, {
+    theme: 'none'
+});
 
 app.mount('#app')
