244db74fdf5acfde34bd48e7b3e7b3e39eb11106
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-05-19 23:32:49 +0600

    tailwind css installed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 
 import Button from 'primevue/button'
 </script>
