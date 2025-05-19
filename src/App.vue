96dc4332e8153edea855e13131965ddd267a2a55
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-05-19 23:53:38 +0600

    login page adding

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,8 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-
-import Button from 'primevue/button'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-<Button label="Save" />
-  <span class="text-4xl">This is Tailwind and Vue Project</span>
-
   <RouterView />
 </template>
 
---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import logo from '@/assets/logo.svg';
+</script>
+
+<template>
+  <main>
+    <div class="login">
+      <h1>Login</h1>
+      <form>
+        <div>
+          <label for="username">Username:</label>
+          <InputText type="text" />
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <InputText type="password" />
+        </div>
+        <Button label="Submit" />
+      </form>
+      <p>Don't have an account? <a href="/register">Register here</a></p>
+    </div>
+    <div>
+      <img :src="logo" alt="logo" width="80" height="80" />
+    </div>
+  </main>
+</template>
