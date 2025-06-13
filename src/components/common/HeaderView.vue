7bc633188bf16becb0d5ae68ffb94d4f90c606f6
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-06-13 12:36:49 +0600

    deployment

---
+++ src/components/common/HeaderView.vue
@@ -0,0 +1,65 @@
+
+<template>
+    <div class="card">
+        <Menubar :model="items">
+            <template #start>
+                <i class="pi pi-spin pi-globe text-success" style="font-size: 2rem"></i>
+            </template>
+            <template #item="{ item, props, hasSubmenu, root }">
+                <a v-ripple class="flex items-center" v-bind="props.action">
+                    <span>{{ item.label }}</span>
+                    <Badge v-if="item.badge" :class="{ 'ml-auto': !root, 'ml-2': root }" :value="item.badge" />
+                    <span v-if="item.shortcut" class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1">{{ item.shortcut }}</span>
+                    <i v-if="hasSubmenu" :class="['pi pi-angle-down ml-auto', { 'pi-angle-down': root, 'pi-angle-right': !root }]"></i>
+                </a>
+            </template>
+            <template #end>
+                <div class="flex items-center gap-2">
+                    <InputText placeholder="Search" type="text" class="w-32 sm:w-auto" />
+                    <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" />
+                </div>
+            </template>
+        </Menubar>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Menubar from 'primevue/menubar';
+import Badge from 'primevue/badge';
+import Avatar from 'primevue/avatar';
+import InputText from 'primevue/inputtext';
+import Ripple from 'primevue/ripple';
+
+const items = ref([
+    {
+        label: 'Home',
+        icon: 'pi pi-home'
+    },
+    {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        badge: 3,
+        items: [
+            {
+                label: 'Core',
+                icon: 'pi pi-bolt',
+                shortcut: '⌘+S'
+            },
+            {
+                label: 'Blocks',
+                icon: 'pi pi-server',
+                shortcut: '⌘+B'
+            },
+            {
+                separator: true
+            },
+            {
+                label: 'UI Kit',
+                icon: 'pi pi-pencil',
+                shortcut: '⌘+U'
+            }
+        ]
+    }
+]);
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import HeaderView from '@/components/common/HeaderView.vue';
 </script>
 
 <template>
-  <main class="flex justify-center items-center min-h-screen">
-    <div>
-      <h1 class="text-3xl">This is home page</h1>
+  <main>
+    <HeaderView />
+    <div class="flex flex-row items-center justify-center h-full p-2">
+      <h1 class="text-center text-4xl text-sky-600 font-bold mt-8">Welcome to Prime Vue</h1>
+      <!-- <div>A</div>
+      <div>B</div> -->
     </div>
+    <!-- <div class="flex flex-row items-center justify-between h-full p-2">
+      <div class="bg-red-500 w-50">Aside</div>
+      <div class="bg-indigo-500 w-50">Body</div>
+    </div> -->
   </main>
 </template>
